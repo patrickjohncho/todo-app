e87b089fa5c5e2391799d68ace57e50659c3dd32
pcho <patrickjohncho@gmail.com>
2023-07-12 11:03:27 -0400

    Test using css styling - better than inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import InlineComponent from './components/inlineComponent';
+import StylesheetComponent from './components/StylesheetComponent';
 
 import Todo from './components/Todo';
 
 function App() {
   return (
     <div className="App">
-      <InlineComponent></InlineComponent>
+      <StylesheetComponent></StylesheetComponent>
     </div>
   );
 }
