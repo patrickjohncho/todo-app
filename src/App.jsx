54391409120d19fe8d02ac9d0421fb931c3a8e45
pcho <patrickjohncho@gmail.com>
2023-07-12 10:57:14 -0400

    Test using inline styles for a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import InlineComponent from './components/inlineComponent';
 
 import Todo from './components/Todo';
 
 function App() {
   return (
     <div className="App">
-      <Todo></Todo>
+      <InlineComponent></InlineComponent>
     </div>
   );
 }
