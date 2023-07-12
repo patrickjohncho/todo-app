2a78dfa887f220bab9f78aa5e521729960e8f7d0
pcho <patrickjohncho@gmail.com>
2023-07-12 15:12:07 -0400

    Able to render todo list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
-import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 import { useState} from 'react';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Form todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList}></Form>
-      <Todo></Todo>
+      <TodoList todoList={todoList}></TodoList>
     </div>
   );
 }
